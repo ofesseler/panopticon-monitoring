306dd795017edd5c67406bfcf533eafd71e2f594
Oliver Fesseler <ofesseler@gmail.com>
2016-12-02 09:26:48 +0100

    renamed salus to panopticon

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 FROM golang:1.7
-
-ADD . $GOPATH/src/fesseler.info/salus/salus
-COPY static/ static/
-
-RUN go install fesseler.info/salus/salus
-
-ENTRYPOINT $GOPATH/bin/salus
+MAINTAINER Oliver Fesseler <oliver@fesseler.info>
 
 EXPOSE 8888
 
+ADD . $GOPATH/src/github.com/panopticon/panopticon
+COPY static/ static/
+
+RUN go install github.com/panopticon/panopticon
+
+ENTRYPOINT $GOPATH/bin/panopticon -prom-host zwuggl:9090
---
+++ panopticon_test.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestGetUniqueLinks(t *testing.T) {
+	links := []Link{
+		{Source: "w1", Target: "w2", Value: 2},
+		{Source: "w2", Target: "w3", Value: 2},
+	}
+	linksDirty := []Link{
+		{Source: "w1", Target: "w2", Value: 1},
+		{Source: "w2", Target: "w3", Value: 1},
+		{Source: "w2", Target: "w1", Value: 1},
+		{Source: "w3", Target: "w2", Value: 1},
+		{Source: "w2", Target: "w2", Value: 1},
+	}
+
+	fn := GetUniqueLinks(linksDirty)
+	if !reflect.DeepEqual(fn, links) {
+		obj, _ := json.MarshalIndent(fn, "", "  ")
+		fmt.Println(string(obj))
+		t.Error("not equal")
+	}
+}
+
+func TestCheckPromResponse(t *testing.T) {
+
+	scrOk := StatusCheckReceived{Status: "success"}
+	scrOk.Data.ResultType = "vector"
+	result := checkPromResponse(scrOk)
+	if result == false {
+		t.Errorf("checkprom respone wasn't successful with struct %v and returned %v", scrOk, result)
+	}
+	scrEmpty := StatusCheckReceived{}
+	result = checkPromResponse(scrEmpty)
+	if result == true {
+		t.Error("sent empty StatusCheckReceived struct and returned true. Shuld be false.")
+	}
+
+	scrResultTypeEmpty := StatusCheckReceived{Status: "success"}
+	result = checkPromResponse(scrResultTypeEmpty)
+	if result == true {
+		t.Error("ResultType was empty and checkPromResponse returned true")
+	}
+
+	scrResultTypeNotVector := StatusCheckReceived{Status: "success"}
+	scrResultTypeNotVector.Data.ResultType = "novector"
+	result = checkPromResponse(scrResultTypeNotVector)
+	if result == true {
+		t.Error("ResultType was NOT 'vector' and checkPromResponse returned true")
+	}
+}
+
+func TestCheckerr(t *testing.T) {
+	checkerr(nil)
+	err := errors.New("type")
+	checkerr(err)
+
+}
